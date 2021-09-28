e657c6c6d02f4834bf1dd00de6c3ff90e7403f1b
kaotisk <kaotisk@arching-kaos.com>
2021-09-28 21:27:41 +0300

    clean up prints, make test function

--- clear.py
+++ clear.py
@@ -1,17 +1,15 @@
 # Encoding
 def encode(m):
     return m[15:31]+"."+m[:15],"."+m[:15]+m[15:31],m[15:31]+m[31:]+".","."+m[:15]+m[31:]+"." 
-
-
+# Decoding
 def decode(tx):
-    print(type(tx))
     ones = []
     twos = []
     tres = []
     fors = []
     vals = []
+    resu = []
     for x in tx:
-        print(x)
         if x.startswith("."): # 12 or 13
             if x.endswith("."): # 13
                 fors.append(x)
@@ -22,11 +20,8 @@ def decode(tx):
                 tres.append(x)
             else:
                 ones.append(x)
-    print("ok")
     for on in ones:
-        print(on)
         for tw in twos:
-            print(tw)
             if on[:16] == tw[16:] or on[16:] == tw[:16]:
                 for tr in tres:
                     if tr[:16] == tw[16:]:
@@ -35,16 +30,21 @@ def decode(tx):
                             if on[16:] == fo[:16]:
                                 print("validated")
                                 vals.append(on[16:]+tw[16:]+tr[16:])
-    return vals
 
-orig = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH"
-pr = encode(orig)
-print(pr)
-e = decode(pr)
-sl = e[0][1:47]
-print(sl)
-print(orig)
-if orig == sl:
-    print("test pasted")
-else:
-    print("failed")
+    for v in vals:
+        if v.startswith(".") and v.endswith("."):
+            resu.append(v[1:47])
+    return resu
+
+def test():
+    orig = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH"
+    pr = encode(orig)
+    print(pr)
+    e = decode(pr)
+    sl = e[0][1:47]
+    print(sl)
+    print(orig)
+    if orig == sl:
+        print("test pasted")
+    else:
+        print("failed")
