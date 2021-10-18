d32c774cd54d6d3cb9d7f5124ada92f4640b6dfb
kaotisk <kaotisk@arching-kaos.com>
2021-10-18 05:45:47 +0300

    Replaced tabs with 4 spaces

--- pack_z_block.sh
+++ pack_z_block.sh
@@ -13,74 +13,74 @@
 FINGERPRINT="$(gpg2 --list-keys |grep kaos@kaos.kaos -1 | head -n1 | awk '{print $1}')"
 
 usage(){
-	echo "$0 - action file"
-	echo "Findout"
+    echo "$0 - action file"
+    echo "Findout"
 }
 
 main(){
  
 echo "We are doing" $ACTION "with content" $MESSAGE
 # We add it to IPFS
-	MESSAGE_HASH=$(ipfs add -q $MESSAGE)
+    MESSAGE_HASH=$(ipfs add -q $MESSAGE)
 
 # We create a detached and armor signature of it
-	MESSAGE_SIGN_FILE=$MESSAGE".asc"
-	gpg2 --detach-sign --sign-with $FINGERPRINT --armor --output $MESSAGE_SIGN_FILE $MESSAGE
+    MESSAGE_SIGN_FILE=$MESSAGE".asc"
+    gpg2 --detach-sign --sign-with $FINGERPRINT --armor --output $MESSAGE_SIGN_FILE $MESSAGE
 
 # We add the signature to IPFS
-	MESSAGE_SIGNATURE=$(ipfs add -q $MESSAGE_SIGN_FILE)
+    MESSAGE_SIGNATURE=$(ipfs add -q $MESSAGE_SIGN_FILE)
 
 # We will be using our public key also to put it in the block later
-	KEY="gpg.pub"
-	gpg2 --armour --output $KEY --export $FINGERPRINT
-	GPG_PUB_KEY=$(ipfs add -q $KEY)
+    KEY="gpg.pub"
+    gpg2 --armour --output $KEY --export $FINGERPRINT
+    GPG_PUB_KEY=$(ipfs add -q $KEY)
 
 # We create a block of json like this:
-	cat > block <<EOF
+    cat > block <<EOF
 {
-	"action":"$ACTION",
-	"data":"$MESSAGE_HASH",
-	"detach":"$MESSAGE_SIGNATURE",
-	"gpg":"$GPG_PUB_KEY"
+    "action":"$ACTION",
+    "data":"$MESSAGE_HASH",
+    "detach":"$MESSAGE_SIGNATURE",
+    "gpg":"$GPG_PUB_KEY"
 }
 EOF
-	BLOCK="block"
-	BLOCK_SIG=$BLOCK".asc"
+    BLOCK="block"
+    BLOCK_SIG=$BLOCK".asc"
 # We have a block now, so we sign it
-	gpg2 --detach-sign --sign-with $FINGERPRINT --armor --output $BLOCK_SIG $BLOCK
+    gpg2 --detach-sign --sign-with $FINGERPRINT --armor --output $BLOCK_SIG $BLOCK
 
 # We now add the signature to IPFS
-	BLOCK_SIGNATURE=$(ipfs add -q $BLOCK_SIG)
+    BLOCK_SIGNATURE=$(ipfs add -q $BLOCK_SIG)
 
 # We also add the block!
-	BLOCK=$(ipfs add -q $BLOCK)
+    BLOCK=$(ipfs add -q $BLOCK)
 
 # So we now do the think almost again
-	cat > zblock << EOF
+    cat > zblock << EOF
 {
-	"block":"$BLOCK",
-	"block_signature":"$BLOCK_SIGNATURE"
+    "block":"$BLOCK",
+    "block_signature":"$BLOCK_SIGNATURE"
 }
 EOF
-	ZBL="zblock"
+    ZBL="zblock"
 # and we add it on IPFS
-	ZBLOCK=$(ipfs add -q $ZBL)
-	echo $ZBLOCK
+    ZBLOCK=$(ipfs add -q $ZBL)
+    echo $ZBLOCK
 }
 title(){
-	echo "AK zblock creator from ACTION and FILE"
-	echo "======================================"
+    echo "AK zblock creator from ACTION and FILE"
+    echo "======================================"
 }
 title
 if [ ! -z $2 ];
 then
-	PWD="$(pwd)"
-	MESSAGE="$PWD/$2"
-	ACTION="$1"
-	main
-	cat $PWD/zblock | json_pp
-	# Optional or extending with
-	# python send_as_ak_tx $ZBLOCK
+    PWD="$(pwd)"
+    MESSAGE="$PWD/$2"
+    ACTION="$1"
+    main
+    cat $PWD/zblock | json_pp
+    # Optional or extending with
+    # python send_as_ak_tx $ZBLOCK
 else usage
 fi
 
