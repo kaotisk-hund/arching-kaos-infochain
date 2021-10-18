7d8afaca6cf553aa36d64fd19db08dedcc520fe8
kaotisk <kaotisk@arching-kaos.com>
2021-10-18 21:11:17 +0300

    Added genesis block detector for empty transaction

--- zExtractor.py
+++ zExtractor.py
@@ -17,7 +17,7 @@ ipfs_gateway="http://127.0.0.1:8080/ipfs/"
 def download_zblock(ZBLOCK_HASH):
     temp=requests.get(ipfs_gateway+ZBLOCK_HASH)
     open(ZBLOCK_HASH, 'wb').write(temp.content)
-    extract_zblock()
+    # extract_zblock()
 
 def download_block(block, block_signature):
     temp=requests.get(ipfs_gateway+block)
@@ -31,11 +31,13 @@ def extract_zblock(ZBLOCK_HASH):
     hash = "zblock"
     hash=ZBLOCK_HASH
     with open(hash) as json_file:
-        data=json.load(json_file)
-        block=data['block']
-        block_signature=data['block_signature']
-        download_block(block,block_signature)
-
+        if os.stat(hash).st_size == 0:
+            print("Genesis block")
+        else:
+            data=json.load(json_file)
+            block=data['block']
+            block_signature=data['block_signature']
+            download_block(block,block_signature)
 
 #def other():
 #    lemq=open(os.path.abspath(".")+"/"+block, "r") 
@@ -45,20 +47,23 @@ def extract_zblock(ZBLOCK_HASH):
 
 def extract_block(block):
     with open(block) as json_file:
-        block_data=json.load(json_file)
-        action=block_data['action']
-        print(action+" called")
-        data=block_data['data']
-        print("for data file: "+data)
-        temp=requests.get(ipfs_gateway+data)
-        open(data, 'wb').write(temp.content)
-        detach=block_data['detach']
-        print("signature: "+detach)
-        temp=requests.get(ipfs_gateway+detach)
-        open(detach, 'wb').write(temp.content)
-        key=block_data['gpg']
-        print("gpg: "+key)
-        open(key, 'wb').write(temp.content)
+        if os.stat(block).st_size == 0:
+            print("Empty block")
+        else:
+            block_data=json.load(json_file)
+            action=block_data['action']
+            print(action+" called")
+            data=block_data['data']
+            print("for data file: "+data)
+            temp=requests.get(ipfs_gateway+data)
+            open(data, 'wb').write(temp.content)
+            detach=block_data['detach']
+            print("signature: "+detach)
+            temp=requests.get(ipfs_gateway+detach)
+            open(detach, 'wb').write(temp.content)
+            key=block_data['gpg']
+            print("gpg: "+key)
+            open(key, 'wb').write(temp.content)
 
 # Example usage:
 # extract_zblock("QmTkWkLkxaF1bwZVEBPB12j4hz9bfD52WGZZLvZaNj9MyD")
