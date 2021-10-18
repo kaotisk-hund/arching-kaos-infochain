22c43360ff7e21ce6bd37e9c04d164dc01e5f59f
kaotisk <kaotisk@arching-kaos.com>
2021-10-18 18:06:34 +0300

    Deleting produced files (zblock, block, block.asc and gpg.pub)

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Cleaning tmp files"
+echo "either way, you are supposed to have them uploaded when you are invoking this"
+echo "or you want to cancel a transaction not already send"
+rm block block.asc gpg.pub zblock
